b1d216d3d7c8621eab28ced1d9efbabb23ae028f
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-09 22:56:25 -0600

    Complete

--- src/test/kotlin/validBinaryNumberTest/BinaryNumberTest.kt
+++ src/test/kotlin/validBinaryNumberTest/BinaryNumberTest.kt
@@ -1,1 +1,24 @@
 package validBinaryNumberTest
+
+import org.example.Verifier
+import org.example.validBinaryNumber.FirstBinaryNumber
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class BinaryNumberTest {
+    @Test
+    fun testBinaryNumber() {
+        val verifier = Verifier()
+
+        assertTrue(verifier.verify("1", FirstBinaryNumber()))
+        assertTrue(verifier.verify("11", FirstBinaryNumber()))
+        assertTrue(verifier.verify("101", FirstBinaryNumber()))
+        assertTrue(verifier.verify("11111111111", FirstBinaryNumber()))
+        assertTrue(verifier.verify("10011010001", FirstBinaryNumber()))
+
+        assertFalse(verifier.verify("01", FirstBinaryNumber()))
+        assertFalse(verifier.verify("1000010", FirstBinaryNumber()))
+        assertFalse(verifier.verify("100a01", FirstBinaryNumber()))
+    }
+}
